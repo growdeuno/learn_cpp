3ba89699d3850999d9dbd8988a8cbe2f7529a93e
growdeuno <grow.deuno@gmail.com>
2018-09-28 03:56:20 +0800

    Post Increment Pre Increment

---
+++ increment.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a = 5;
+  // Post Increment
+  int b = a++;
+  cout << b << endl;
+
+  // Pre Increment
+  int x = 10;
+  int y = ++x;
+
+  cout << y << endl;
+
+
+
+
+
+}
