b3b32581aa713df363caf115008e7a6a706bbadb
growdeuno <grow.deuno@gmail.com>
2018-09-28 03:40:50 +0800

    Sentence with Endline

---
+++ endline.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  string sentence;
+  sentence = "I attempt to use sentence with endline";
+
+  cout << sentence << endl;
+  return 0;
+}
